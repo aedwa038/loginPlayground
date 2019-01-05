bc73490c4f2572136f51f9a1c26c4cfe71c04241
akeemedwards <akeemedwards@google.com>
2019-01-04 22:25:01 -0500

    removed docker pushing for login app

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 echo "Building Database"
 cd login-playground-database &&  sh ./build.sh && cd ../
 echo "Building Login App"
-cd login-playground-app && mvn clean install docker:build docker:push && cd ../
+cd login-playground-app && mvn clean install docker:build  && cd ../
 echo "Building Docs"
 cd login-playground-docs && sh ./build.sh && cd ../
 echo "Done"
