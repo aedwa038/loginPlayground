9ac58f812dca4c3446453010c266e7938dd162ff
akeemedwards <akeemedwards@google.com>
2019-01-04 22:15:20 -0500

    more travis ci stuff

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "Building Database"
 cd login-playground-database &&  sh ./build.sh && cd ../
 echo "Building Login App"
-cd login-playground-app && mvn clean install docker:build && cd ../
+cd login-playground-app && mvn clean install docker:build docker:push && cd ../
 echo "Building Docs"
 cd login-playground-docs && sh ./build.sh && cd ../
 echo "Done"
