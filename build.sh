fe6770e1579c72e6cbf317509af923be41fa0c7f
akeemedwards <akeemedwards@google.com>
2019-01-10 17:51:52 -0500

    updated build files

--- build.sh
+++ build.sh
@@ -8,9 +8,5 @@ cd login-playground-docs && sh ./build.sh && cd ../
 echo "Publising to Docker hub"
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 #echo "$DOCKER_PASSWORD"
-#docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD ;
-docker push $DOCKER_USERNAME/playground-docs:latest
-docker push $DOCKER_USERNAME/login_db:latest
-docker push $DOCKER_USERNAME/login-playground:latest
 echo "Done"
 #docker-compose down && cd login-playground-app/ && mvn clean install docker:build && cd ../ && docker-compose up
